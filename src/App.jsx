8e0b39e964cb74dea90744c055e1dd3c22db2047
Jason <jason.jafari.m@gmail.com>
2020-07-14 10:40:22 -0400

    add 1 query from server

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import  from ''
+import ApolloClient from 'apollo-boost';
+
+import gql from'graphql-tag'
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000/"
+})
+
+client
+    .query({
+        query: gql`
+            {
+                recipes {
+                    id
+                    title
+                }
+            }
+        `
+    })
+    .then(result=> console.log(result))
 
 class App extends Component {
-     
-    render() { 
-        return ( 
+
+    render() {
+        return (
             <div>
                 Hello World
             </div>
-         );
+        );
     }
 }
- 
+
 export default App;
